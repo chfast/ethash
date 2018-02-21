35ac44ec2dcce1cb2dbaec268f344334a108dbec
Paweł Bylica <chfast@gmail.com>
2018-02-22 00:11:39 +0100

    Init ethash library project
    Licensed under the Apache License, Version 2.0.

---
+++ lib/ethash/utils.cpp
@@ -0,0 +1,24 @@
+// Copyright 2018 Pawel Bylica.
+// Licensed under the Apache License, Version 2.0. See the LICENSE file.
+
+#include "utils.hpp"
+
+namespace ethash
+{
+constexpr bool is_prime(size_t number) noexcept
+{
+    if (number <= 1)
+        return false;
+
+    if (number % 2 == 0 && number > 2)
+        return false;
+
+    for (size_t d = 3; (d * d) < number; d += 2)
+    {
+        if (number % d == 0)
+            return false;
+    }
+
+    return true;
+}
+}
---
+++ lib/ethash/utils.hpp
@@ -0,0 +1,11 @@
+// Copyright 2018 Pawel Bylica.
+// Licensed under the Apache License, Version 2.0. See the LICENSE file.
+
+#pragma once
+
+#include <cstddef>
+
+namespace ethash
+{
+constexpr bool is_prime(size_t number) noexcept;
+}
