1d0bcb9ae2f5d0eb608106b508765f9cdb4c5525
Paweł Bylica <chfast@gmail.com>
2018-03-29 16:31:09 +0200

    Compress ethash unit tests

--- test/unittests/test_ethash.cpp
+++ test/unittests/test_ethash.cpp
@@ -40,189 +40,115 @@ epoch_context create_epoch_context_mock(int epoch_number)
 TEST(hash, hash1024_init)
 {
     ethash::hash1024 hash;
-    ASSERT_EQ(hash.words[0], 0);
-    ASSERT_EQ(hash.words[1], 0);
-    ASSERT_EQ(hash.words[14], 0);
-    ASSERT_EQ(hash.words[15], 0);
+    for (auto w : hash.words)
+        EXPECT_EQ(w, 0);
 }
 
-
-class calculate_light_cache_size_test
-  : public ::testing::TestWithParam<std::pair<int, uint64_t>>
-{
-};
-
-class calculate_full_dataset_size_test
-  : public ::testing::TestWithParam<std::pair<int, uint64_t>>
-{
-};
-
-TEST_P(calculate_light_cache_size_test, test)
+struct dataset_size_test_case
 {
     int epoch_number;
-    uint64_t expected_size;
-    std::tie(epoch_number, expected_size) = GetParam();
-
-    uint64_t size = ethash::calculate_light_cache_size(epoch_number);
-    EXPECT_EQ(size, expected_size);
-}
-
-/// Test pairs for calculate_light_cache_size() are taken from generated ethash
-/// light cache sizes, picked at random.
-/// See https://github.com/ethereum/wiki/wiki/Ethash#data-sizes.
-static std::pair<int, uint64_t> light_cache_sizes[] = {
-    {0, 16776896},
-    {14, 18611392},
-    {17, 19004224},
-    {56, 24116672},
-    {158, 37486528},
-    {203, 43382848},
-    {211, 44433344},
-    {272, 52427968},
-    {350, 62651584},
-    {412, 70778816},
-    {464, 77593664},
-    {530, 86244416},
-    {656, 102760384},
-    {657, 102890432},
-    {658, 103021888},
-    {739, 113639104},
-    {751, 115212224},
-    {798, 121372352},
-    {810, 122945344},
-    {862, 129760832},
-    {882, 132382528},
-    {920, 137363392},
-    {977, 144832448},
-    {1093, 160038464},
-    {1096, 160430656},
-    {1119, 163446592},
-    {1125, 164233024},
-    {1165, 169475648},
-    {1168, 169866304},
-    {1174, 170655424},
-    {1211, 175504832},
-    {1244, 179830208},
-    {1410, 201588544},
-    {1418, 202636352},
-    {1436, 204996544},
-    {1502, 213645632},
-    {1512, 214956992},
-    {1535, 217972672},
-    {1538, 218364736},
-    {1554, 220461632},
-    {1571, 222690368},
-    {1602, 226754368},
-    {1621, 229243328},
-    {1630, 230424512},
-    {1698, 239335232},
-    {1746, 245628736},
-    {1790, 251395136},
-    {1818, 255065792},
-    {1912, 267386432},
-    {1928, 269482688},
-    {1956, 273153856},
-    {2047, 285081536},
+    uint64_t light_cache_size;
+    uint64_t full_dataset_size;
 };
-INSTANTIATE_TEST_CASE_P(
-    light_cache_sizes, calculate_light_cache_size_test, testing::ValuesIn(light_cache_sizes));
 
+/// Test cases for dataset sizes are random picked from generated ethash sizes.
+/// See https://github.com/ethereum/wiki/wiki/Ethash#data-sizes.
+static dataset_size_test_case dataset_size_test_cases[] = {
+    {0, 16776896, 1073739904},
+    {14, 18611392, 1191180416},
+    {17, 19004224, 1216345216},
+    {56, 24116672, 1543503488},
+    {158, 37486528, 2399139968},
+    {203, 43382848, 2776625536},
+    {211, 44433344, 2843734144},
+    {272, 52427968, 3355440512},
+    {350, 62651584, 4009751168},
+    {412, 70778816, 4529846144},
+    {464, 77593664, 4966054784},
+    {530, 86244416, 5519703424},
+    {656, 102760384, 6576662912},
+    {657, 102890432, 6585055616},
+    {658, 103021888, 6593443456},
+    {739, 113639104, 7272921472},
+    {751, 115212224, 7373585792},
+    {798, 121372352, 7767849088},
+    {810, 122945344, 7868512384},
+    {862, 129760832, 8304715136},
+    {882, 132382528, 8472492928},
+    {920, 137363392, 8791260032},
+    {977, 144832448, 9269411456},
+    {1093, 160038464, 10242489472},
+    {1096, 160430656, 10267656064},
+    {1119, 163446592, 10460589952},
+    {1125, 164233024, 10510923392},
+    {1165, 169475648, 10846469248},
+    {1168, 169866304, 10871631488},
+    {1174, 170655424, 10921964672},
+    {1211, 175504832, 11232345728},
+    {1244, 179830208, 11509169024},
+    {1410, 201588544, 12901672832},
+    {1418, 202636352, 12968786816},
+    {1436, 204996544, 13119782528},
+    {1502, 213645632, 13673430656},
+    {1512, 214956992, 13757316224},
+    {1535, 217972672, 13950253696},
+    {1538, 218364736, 13975417216},
+    {1554, 220461632, 14109635968},
+    {1571, 222690368, 14252243072},
+    {1602, 226754368, 14512291712},
+    {1621, 229243328, 14671675264},
+    {1630, 230424512, 14747172736},
+    {1698, 239335232, 15317596544},
+    {1746, 245628736, 15720251264},
+    {1790, 251395136, 16089346688},
+    {1818, 255065792, 16324230016},
+    {1912, 267386432, 17112759424},
+    {1928, 269482688, 17246976896},
+    {1956, 273153856, 17481857408},
+    {2047, 285081536, 18245220736},
+};
 
-TEST_P(calculate_full_dataset_size_test, test)
+TEST(ethash, light_cache_size)
 {
-    int epoch_number;
-    uint64_t expected_size;
-    std::tie(epoch_number, expected_size) = GetParam();
-
-    uint64_t size = ethash::calculate_full_dataset_size(epoch_number);
-    EXPECT_EQ(size, expected_size);
+    for (const auto& t : dataset_size_test_cases)
+    {
+        uint64_t size = ethash::calculate_light_cache_size(t.epoch_number);
+        EXPECT_EQ(size, t.light_cache_size) << "epoch: " << t.epoch_number;
+    }
 }
 
-/// Test pairs for calculate_full_dataset_size() are taken from generated ethash
-/// full dataset sizes, picked at random.
-/// See https://github.com/ethereum/wiki/wiki/Ethash#data-sizes.
-static std::pair<int, uint64_t> full_dataset_sizes[] = {
-    {0, 1073739904},
-    {14, 1191180416},
-    {17, 1216345216},
-    {56, 1543503488},
-    {158, 2399139968},
-    {203, 2776625536},
-    {211, 2843734144},
-    {272, 3355440512},
-    {350, 4009751168},
-    {412, 4529846144},
-    {464, 4966054784},
-    {530, 5519703424},
-    {656, 6576662912},
-    {657, 6585055616},
-    {658, 6593443456},
-    {739, 7272921472},
-    {751, 7373585792},
-    {798, 7767849088},
-    {810, 7868512384},
-    {862, 8304715136},
-    {882, 8472492928},
-    {920, 8791260032},
-    {977, 9269411456},
-    {1093, 10242489472},
-    {1096, 10267656064},
-    {1119, 10460589952},
-    {1125, 10510923392},
-    {1165, 10846469248},
-    {1168, 10871631488},
-    {1174, 10921964672},
-    {1211, 11232345728},
-    {1244, 11509169024},
-    {1410, 12901672832},
-    {1418, 12968786816},
-    {1436, 13119782528},
-    {1502, 13673430656},
-    {1512, 13757316224},
-    {1535, 13950253696},
-    {1538, 13975417216},
-    {1554, 14109635968},
-    {1571, 14252243072},
-    {1602, 14512291712},
-    {1621, 14671675264},
-    {1630, 14747172736},
-    {1698, 15317596544},
-    {1746, 15720251264},
-    {1790, 16089346688},
-    {1818, 16324230016},
-    {1912, 17112759424},
-    {1928, 17246976896},
-    {1956, 17481857408},
-    {2047, 18245220736},
-};
-INSTANTIATE_TEST_CASE_P(
-    full_dataset_sizes, calculate_full_dataset_size_test, testing::ValuesIn(full_dataset_sizes));
+TEST(ethash, full_dataset_size)
+{
+    for (const auto& t : dataset_size_test_cases)
+    {
+        uint64_t size = ethash::calculate_full_dataset_size(t.epoch_number);
+        EXPECT_EQ(size, t.full_dataset_size) << "epoch: " << t.epoch_number;
+    }
+}
 
 
-TEST(calculate_seed_test, zero)
+TEST(ethash, calculate_seed_0)
 {
     hash256 s0 = calculate_seed(0);
-    EXPECT_EQ(s0.words[0], 0);
-    EXPECT_EQ(s0.words[1], 0);
-    EXPECT_EQ(s0.words[2], 0);
-    EXPECT_EQ(s0.words[3], 0);
+    for (auto w : s0.words)
+        EXPECT_EQ(w, 0);
 }
 
-TEST(calculate_seed_test, one)
+TEST(ethash, calculate_seed_1)
 {
     hash256 seed = calculate_seed(1);
     std::string sh = to_hex(seed);
     EXPECT_EQ(sh, "290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563");
 }
 
-TEST(calculate_seed_test, current)
+TEST(ethash, calculate_seed_171)
 {
     hash256 seed = calculate_seed(171);
     std::string sh = to_hex(seed);
     EXPECT_EQ(sh, "a9b0e0c9aca72c07ba06b5bbdae8b8f69e61878301508473379bb4f71807d707");
 }
 
-TEST(calculate_seed_test, last)
+TEST(ethash, calculate_seed_2018)
 {
     hash256 seed = calculate_seed(2048);
     std::string sh = to_hex(seed);
@@ -242,7 +168,7 @@ TEST(ethash, get_epoch_number)
     EXPECT_EQ(get_epoch_number(29999), 0);
     EXPECT_EQ(get_epoch_number(30000), 1);
     EXPECT_EQ(get_epoch_number(30001), 1);
-    EXPECT_EQ(get_epoch_number(30001), 1);
+    EXPECT_EQ(get_epoch_number(30002), 1);
     EXPECT_EQ(get_epoch_number(5000000), 166);
 }
 
