f63217ee592f085a9ca37aff193b3f0ca8437849
Paweł Bylica <chfast@gmail.com>
2018-04-03 12:06:09 +0200

    Add integration test for exported CMake config

---
+++ test/integration/cmake-config/cmake_config_test.cpp
@@ -0,0 +1,11 @@
+// Copyright 2018 Pawel Bylica.
+// Licensed under the Apache License, Version 2.0. See the LICENSE file.
+
+#include <ethash/ethash.hpp>
+
+int main()
+{
+    uint8_t seed_bytes[32] = {0};
+    ethash::hash256 seed = ethash::hash256::from_bytes(seed_bytes);
+    return ethash::find_epoch_number(seed);
+}
