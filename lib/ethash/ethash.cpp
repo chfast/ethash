a149ee865be333afa7ee7a8a971924ba8725d268
Paweł Bylica <chfast@gmail.com>
2018-04-17 15:31:11 +0200

    Allow unsigned integer overflow in fnv()

--- lib/ethash/ethash.cpp
+++ lib/ethash/ethash.cpp
@@ -12,6 +12,17 @@
 #include "params.hpp"
 #include "utils.hpp"
 
+#if defined(__has_attribute)
+#if __has_attribute(no_sanitize)
+#define ATTRIBUTE_NO_SANITIZE_UNSIGNED_INTEGER_OVERFLOW \
+    __attribute__((no_sanitize("unsigned-integer-overflow")))
+#endif
+#endif
+
+#if !defined(ATTRIBUTE_NO_SANITIZE_UNSIGNED_INTEGER_OVERFLOW)
+#define ATTRIBUTE_NO_SANITIZE_UNSIGNED_INTEGER_OVERFLOW
+#endif
+
 namespace ethash
 {
 // Verify constants:
@@ -22,6 +33,7 @@ static_assert(full_dataset_item_size == ETHASH_FULL_DATASET_ITEM_SIZE, "");
 
 namespace
 {
+ATTRIBUTE_NO_SANITIZE_UNSIGNED_INTEGER_OVERFLOW
 inline uint32_t fnv(uint32_t u, uint32_t v) noexcept
 {
     return (u * 0x01000193) ^ v;
