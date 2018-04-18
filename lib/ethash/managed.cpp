6895b227ca434696c93c47a4effa5c1b2c2b3aef
Paweł Bylica <chfast@gmail.com>
2018-04-18 12:32:19 +0200

    Extract slow path of get_epoch_context to a noinline function

--- lib/ethash/managed.cpp
+++ lib/ethash/managed.cpp
@@ -6,6 +6,18 @@
 #include <memory>
 #include <mutex>
 
+#if !defined(__has_cpp_attribute)
+#define __has_cpp_attribute(x) 0
+#endif
+
+#if __has_cpp_attribute(gnu::noinline)
+#define ATTRIBUTE_NOINLINE [[gnu::noinline]]
+#elif _MSC_VER
+#define ATTRIBUTE_NOINLINE __declspec(noinline)
+#else
+#define ATTRIBUTE_NOINLINE
+#endif
+
 namespace ethash
 {
 namespace managed
@@ -15,31 +27,40 @@ namespace
 std::mutex shared_context_mutex;
 std::shared_ptr<ethash_epoch_context> shared_context;
 thread_local std::shared_ptr<ethash_epoch_context> thread_local_context;
+
+/// Update thread local epoch context.
+///
+/// This function is on the slow path. It's separated to allow inlining the fast
+/// path.
+///
+/// @todo: Check noexpect.
+ATTRIBUTE_NOINLINE
+void update_local_context(int epoch_number)
+{
+    // Release the shared pointer of the obsoleted context.
+    thread_local_context.reset();
+
+    // Local context invalid, check the shared context.
+    std::lock_guard<std::mutex> lock{shared_context_mutex};
+
+    if (!shared_context || shared_context->epoch_number != epoch_number)
+    {
+        // Release the shared pointer of the obsoleted context.
+        shared_context.reset();
+
+        // Build new context.
+        shared_context = {ethash_create_epoch_context(epoch_number), ethash_destroy_epoch_context};
+    }
+
+    thread_local_context = shared_context;
+}
 }  // namespace
 
 const ethash_epoch_context& get_epoch_context(int epoch_number)
 {
     // Check if local context matches epoch number.
     if (!thread_local_context || thread_local_context->epoch_number != epoch_number)
-    {
-        // Release the shared pointer of the obsoleted context.
-        thread_local_context.reset();
-
-        // Local context invalid, check the shared context.
-        std::lock_guard<std::mutex> lock{shared_context_mutex};
-
-        if (!shared_context || shared_context->epoch_number != epoch_number)
-        {
-            // Release the shared pointer of the obsoleted context.
-            shared_context.reset();
-
-            // Build new context.
-            shared_context = {
-                ethash_create_epoch_context(epoch_number), ethash_destroy_epoch_context};
-        }
-
-        thread_local_context = shared_context;
-    }
+        update_local_context(epoch_number);
 
     return *thread_local_context;
 }
