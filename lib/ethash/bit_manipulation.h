2114627d4eee9470a63c6b58e83790f489d4da8d
Paweł Bylica <chfast@gmail.com>
2018-09-26 23:06:54 +0200

    Add bit manipulation helper functions

---
+++ lib/ethash/bit_manipulation.h
@@ -0,0 +1,46 @@
+/* ethash: C/C++ implementation of Ethash, the Ethereum Proof of Work algorithm.
+ * Copyright 2018 Pawel Bylica.
+ * Licensed under the Apache License, Version 2.0. See the LICENSE file.
+ */
+
+#pragma once
+
+#include "builtins.h"
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static inline uint32_t rotl32(uint32_t n, unsigned int c)
+{
+    const unsigned int mask = 31;
+
+    c &= mask;
+    unsigned int neg_c = (unsigned int)(-(int)c);
+    return (n << c) | (n >> (neg_c & mask));
+}
+
+static inline uint32_t rotr32(uint32_t n, unsigned int c)
+{
+    const unsigned int mask = 31;
+
+    c &= mask;
+    unsigned int neg_c = (unsigned int)(-(int)c);
+    return (n >> c) | (n << (neg_c & mask));
+}
+
+static inline uint32_t clz32(uint32_t x)
+{
+    return x ? (uint32_t)__builtin_clz(x) : 32;
+}
+
+static inline uint32_t popcount32(uint32_t x)
+{
+    return (uint32_t)__builtin_popcount(x);
+}
+
+#ifdef __cplusplus
+}
+#endif
