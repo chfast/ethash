2107b173d325bd20efba6afd217787b5dc89b9d2
Paweł Bylica <chfast@gmail.com>
2018-09-27 17:09:37 +0200

    Squashed 'cmake/cable/' changes from a5a1829..c528da4
    c528da4 README: Add information how to update git subtree
    311c059 Cable 0.2.13: Fix spelling typos
    d4de521 Cable 0.2.12
    646c412 gitinfo: Use --first-parent for git-describe
    6458a65 Cable 0.2.11
    febf8ad buildinfo: Make buildinfo library installable
    a784149 buildinfo: Handle undefined variables
    8484c27 Cable 0.2.10: Add PIC toolchain files
    c52a405 Cable 0.2.9: Fix gitinfo when no git
    912f5e1 Cable 0.2.8
    3ed70c6 buildinfo: Mark buildinfo.json as GENERATED file
    a0c88f9 Cable 0.2.7
    94bf242 buildinfo: Export buildinfo.json file
    c013495 gitinfo: Include branch name and origin URL
    36b6cb6 Cable 0.2.6
    f74ad95 buildinfo: Clean up output directory
    6babdd4 buildinfo: Export some information for shell scripts
    ceb5964 Cable 0.2.5: Extend buildinfo with project name
    28805f8 Cable 0.2.4: Fix --coverage detection
    268fdf7 CompilerSettings: Fix error message for unsupported --coverage
    75766fd Git ignore CLion files
    cbca43b Cable 0.2.3: Allow loading toolchain files from outside of cable dir
    caeabb3 CableBuildInfo: Specify gitinfo.txt byproduct
    69a5abc CableBuildInfo: Remove broken debug logs
    
    git-subtree-dir: cmake/cable
    git-subtree-split: c528da4a35286cd7cd32cdc2d01edba887af8852

--- buildinfo/buildinfo.cmake
+++ buildinfo/buildinfo.cmake
@@ -2,21 +2,29 @@
 # Copyright 2018 Pawel Bylica.
 # Licensed under the Apache License, Version 2.0. See the LICENSE file.
 
-string(TOLOWER ${SYSTEM_NAME} SYSTEM_NAME)
-string(TOLOWER ${SYSTEM_PROCESSOR} SYSTEM_PROCESSOR)
-string(TOLOWER ${COMPILER_ID} COMPILER_ID)
-string(TOLOWER ${BUILD_TYPE} BUILD_TYPE)
+string(TOLOWER "${SYSTEM_NAME}" SYSTEM_NAME)
+string(TOLOWER "${SYSTEM_PROCESSOR}" SYSTEM_PROCESSOR)
+string(TOLOWER "${COMPILER_ID}" COMPILER_ID)
+string(TOLOWER "${BUILD_TYPE}" BUILD_TYPE)
 string(TIMESTAMP TIMESTAMP)
 
 # Read the git info from a file. The gitinfo is suppose to update the file
 # only if the information has changed.
-file(READ ${BINARY_DIR}/gitinfo.txt GIT_INFO)
+file(STRINGS ${OUTPUT_DIR}/gitinfo.txt gitinfo)
+list(LENGTH gitinfo gitinfo_len)
+if(gitinfo_len LESS 3)
+    message(WARNING "Git info not available")
+else()
+    list(GET gitinfo 0 describe)
+    list(GET gitinfo 1 GIT_BRANCH)
+    list(GET gitinfo 2 GIT_ORIGIN_URL)
+endif()
 
 # The output of `git describe --always --long --tags --match=v*`.
-string(REGEX MATCH "(v(.+)-([0-9]+)-g)?([0-9a-f]+)(-dirty)?" match "${GIT_INFO}")
+string(REGEX MATCH "(v(.+)-([0-9]+)-g)?([0-9a-f]+)(-dirty)?" match "${describe}")
 
-if(NOT match)
-    message(WARNING "Cannot parse git describe: ${GIT_INFO}")
+if(DEFINED describe AND NOT match)
+    message(WARNING "Cannot parse git describe: ${describe}")
 endif()
 
 set(GIT_LATEST_PROJECT_VERSION ${CMAKE_MATCH_2})
@@ -29,13 +37,6 @@ else()
     set(GIT_DIRTY FALSE)
 endif()
 
-cable_debug("gitinfo: parsing '${GIT_INFO}':")
-cable_debug("  version:  ${GIT_LATEST_PROJECT_VERSION}")
-cable_debug("  distance: ${GIT_LATEST_PROJECT_VERSION_DISTANCE}")
-cable_debug("  commit:   ${GIT_COMMIT_HASH}")
-cable_debug("  dirty:    ${GIT_DIRTY}")
-
-
 if(GIT_COMMIT_HASH)
     string(SUBSTRING ${GIT_COMMIT_HASH} 0 8 abbrev)
     set(version_commit "+commit.${abbrev}")
@@ -44,7 +45,11 @@ if(GIT_COMMIT_HASH)
     endif()
 endif()
 
-if(${PROJECT_VERSION} STREQUAL "${GIT_LATEST_PROJECT_VERSION}")
+if(NOT PROJECT_VERSION)
+    message(WARNING "PROJECT_VERSION not specified")
+endif()
+
+if(PROJECT_VERSION STREQUAL GIT_LATEST_PROJECT_VERSION)
     if(${GIT_LATEST_PROJECT_VERSION_DISTANCE} GREATER 0)
         set(PROJECT_VERSION "${PROJECT_VERSION}-${GIT_LATEST_PROJECT_VERSION_DISTANCE}${version_commit}")
     endif()
@@ -55,8 +60,15 @@ else()
     set(PROJECT_VERSION "${PROJECT_VERSION}${version_commit}")
 endif()
 
+if(PROJECT_VERSION MATCHES "^([0-9]+)\\.([0-9]+)\\.([0-9]+)$")
+    set(PROJECT_VERSION_IS_PRERELEASE false)
+else()
+    set(PROJECT_VERSION_IS_PRERELEASE true)
+    set(prerelease_comment " (prerelease)")
+endif()
+
 message(
-    "       Project Version:  ${PROJECT_VERSION}\n"
+    "       Project Version:  ${PROJECT_VERSION}${prerelease_comment}\n"
     "       System Name:      ${SYSTEM_NAME}\n"
     "       System Processor: ${SYSTEM_PROCESSOR}\n"
     "       Compiler ID:      ${COMPILER_ID}\n"
@@ -66,4 +78,7 @@ message(
     "       Timestamp:        ${TIMESTAMP}"
 )
 
-configure_file(${CMAKE_CURRENT_LIST_DIR}/buildinfo.c.in ${BINARY_DIR}/${NAME}.c)
+configure_file(${CMAKE_CURRENT_LIST_DIR}/buildinfo.c.in ${OUTPUT_DIR}/buildinfo.c)
+configure_file(${CMAKE_CURRENT_LIST_DIR}/buildinfo.json.in ${OUTPUT_DIR}/buildinfo.json)
+configure_file(${CMAKE_CURRENT_LIST_DIR}/buildinfo.sh.in ${OUTPUT_DIR}/buildinfo.sh)
+configure_file(${CMAKE_CURRENT_LIST_DIR}/buildinfo.ps1.in ${OUTPUT_DIR}/buildinfo.ps1)
