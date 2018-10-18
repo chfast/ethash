2b4892e2ba94ddd7dd671e49a28ff71a54530163
Paweł Bylica <chfast@gmail.com>
2018-10-18 12:52:09 +0200

    progpow: Test keccak_progpow_64() comparison

--- test/unittests/test_progpow.cpp
+++ test/unittests/test_progpow.cpp
@@ -2,6 +2,7 @@
 // Copyright 2018 Pawel Bylica.
 // Licensed under the Apache License, Version 2.0. See the LICENSE file.
 
+#include <ethash/endianness.hpp>
 #include <ethash/progpow-internal.hpp>
 
 #include "helpers.hpp"
@@ -9,6 +10,12 @@
 
 #include <gtest/gtest.h>
 
+
+TEST(progpow, keccak_progpow_256)
+{
+    const auto h = progpow::keccak_progpow_256({}, 0, {});
+    EXPECT_EQ(to_hex(h), "5dd431e5fbc604f499bfa0232f45f8f142d0ff5178f539e5a7800bf0643697af");
+}
 
 TEST(progpow, keccak_progpow_64)
 {
@@ -30,10 +37,26 @@ TEST(progpow, keccak_progpow_64)
     EXPECT_EQ(h2, 0x824c43b52688dd18);
 }
 
-TEST(progpow, keccak_progpow_256)
+TEST(progpow, keccak_progpow_64boundary)
 {
-    const auto h = progpow::keccak_progpow_256({}, 0, {});
-    EXPECT_EQ(to_hex(h), "5dd431e5fbc604f499bfa0232f45f8f142d0ff5178f539e5a7800bf0643697af");
+    using namespace progpow;
+
+    const hash256 header_hash{};
+    const uint64_t nonce = 254984491;
+    const hash256 boundary =
+        to_hash256("0000000f00000000000000000000000000000000000000000000000000000000");
+    const uint64_t boundary_prefix = be::uint64(boundary.words[0]);
+
+    hash256 h = keccak_progpow_256(header_hash, nonce, {});
+    EXPECT_EQ(to_hex(h), "00000002e29488b39928408765b4645343fd9dd7ed2a4d4ddf42bf1c19ff8bac");
+
+    // The full hash is smaller than the boundary hash:
+    EXPECT_LT(std::memcmp(h.bytes, boundary.bytes, sizeof(h)), 0);
+
+    // The approximated comparison can be done against 64-bit prefix.
+    uint64_t s = keccak_progpow_64(header_hash, nonce, {});
+    EXPECT_EQ(s, 0x00000002e29488b3);
+    EXPECT_LT(s, boundary_prefix);
 }
 
 TEST(progpow, mix_rng_state)
