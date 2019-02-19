1d5ca8825d13705511e468c7e2c125481fb0927b
Paweł Bylica <chfast@gmail.com>
2019-02-19 15:09:47 +0100

    Add version.h header with information about the library version

--- test/integration/cmake-config/cmake_config_test.cpp
+++ test/integration/cmake-config/cmake_config_test.cpp
@@ -3,9 +3,12 @@
 // Licensed under the Apache License, Version 2.0. See the LICENSE file.
 
 #include <ethash/ethash.hpp>
+#include <ethash/version.h>
 
 int main()
 {
+    static_assert(sizeof(ethash::version) >= 6, "incorrect ethash::version");
+
     uint8_t seed_bytes[32] = {0};
     ethash::hash256 seed = ethash::hash256_from_bytes(seed_bytes);
     return ethash::find_epoch_number(seed);
