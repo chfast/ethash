4184ab499d079391db848c1b9030aa9584e55cf6
Paweł Bylica <chfast@gmail.com>
2019-06-11 15:35:19 +0200

    ci: Build Python wheels on linux and macos

--- scripts/ci/python_build_wheels.sh
+++ scripts/ci/python_build_wheels.sh
@@ -1,24 +1,33 @@
 #!/usr/bin/env bash
+# -*- coding: utf-8 -*-
 
 # ethash: C/C++ implementation of Ethash, the Ethereum Proof of Work algorithm.
 # Copyright 2019 Pawel Bylica.
 # Licensed under the Apache License, Version 2.0.
 
-set -e
+set -eo pipefail
 
 if [ -n "$APPVEYOR" ]; then
-    PYTHON_PATHS="/c/Python37-x64:/c/Python37-x64/Scripts /c/Python36-x64:/c/Python36-x64/Scripts /c/Python35-x64:/c/Python35-x64/Scripts"
+    PYTHON_PATHS="/c/Python37-x64 /c/Python36-x64 /c/Python35-x64"
+elif [ -n "$CIRCLECI" ]; then
+    if [ "$OSTYPE" = "linux-gnu" ]; then
+        PYTHON_PATHS="/opt/python/cp37-cp37m/bin /opt/python/cp36-cp36m/bin /opt/python/cp35-cp35m/bin"
+    else
+        ln -s /usr/local/Cellar/python/3.7.3/bin/python3 /usr/local/Cellar/python/3.7.3/bin/python
+        PYTHON_PATHS="/usr/local/Cellar/python/3.7.3/bin"
+    fi
 fi
 
 PATH_ORIG=$PATH
 for p in $PYTHON_PATHS
 do
-    export PATH="$p:$PATH_ORIG"
+    PATH="$p:$PATH_ORIG"
     echo '***'
     python --version
+    which python
+    python -m pip --version
     echo '***'
-    pip --version
-    pip install wheel
+    python -m pip install wheel
     python setup.py build_ext --skip-cmake-build
     python setup.py bdist_wheel --skip-build
 done
