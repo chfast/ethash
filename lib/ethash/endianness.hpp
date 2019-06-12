191194565033ff71e29ecfdb3215b2788f26368a
Paweł Bylica <chfast@gmail.com>
2019-06-12 11:55:13 +0200

    Bump year in the copyright notice

--- lib/ethash/endianness.hpp
+++ lib/ethash/endianness.hpp
@@ -1,5 +1,6 @@
-// Copyright 2018 Pawel Bylica.
-// Licensed under the Apache License, Version 2.0. See the LICENSE file.
+// ethash: C/C++ implementation of Ethash, the Ethereum Proof of Work algorithm.
+// Copyright 2018-2019 Pawel Bylica.
+// Licensed under the Apache License, Version 2.0.
 
 /// @file
 /// This file contains helper functions to handle big-endian architectures.
