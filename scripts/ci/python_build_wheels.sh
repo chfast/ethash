3644e587ca806c4c2b44a57154343c4373529827
Paweł Bylica <pawel@ethereum.org>
2023-09-07 22:53:40 +0200

    ci: Upgrade Python build config
    Upgrade image for building wheels to manylinux_2_28_x86_64
    and build ethash C library in the same container.

--- scripts/ci/python_build_wheels.sh
+++ scripts/ci/python_build_wheels.sh
@@ -7,11 +7,14 @@
 
 set -eo pipefail
 
+ACTIVATE_DIR=bin
+
 if [ -n "$APPVEYOR" ]; then
-    PYTHON_PATHS="/c/Python310-x64:/c/Python310-x64/Scripts /c/Python39-x64:/c/Python39-x64/Scripts /c/Python38-x64:/c/Python38-x64/Scripts /c/Python37-x64:/c/Python37-x64/Scripts"
+    ACTIVATE_DIR=Scripts
+    PYTHON_PATHS="/c/Python311-x64:/c/Python311-x64/Scripts /c/Python310-x64:/c/Python310-x64/Scripts /c/Python39-x64:/c/Python39-x64/Scripts /c/Python38-x64:/c/Python38-x64/Scripts"
 elif [ -n "$CIRCLECI" ]; then
     if [ "$OSTYPE" = "linux-gnu" ]; then
-        PYTHON_PATHS="/opt/python/cp310-cp310/bin /opt/python/cp39-cp39/bin /opt/python/cp38-cp38/bin /opt/python/cp37-cp37m/bin /opt/python/cp36-cp36m/bin"
+        PYTHON_PATHS="/opt/python/cp311-cp311/bin /opt/python/cp310-cp310/bin /opt/python/cp39-cp39/bin /opt/python/cp38-cp38/bin"
     else
         # The continuation of the script expects "python" executable name,
         # so make link python -> python3.
@@ -25,12 +28,16 @@ PATH_ORIG=$PATH
 for p in $PYTHON_PATHS
 do
     PATH="$p:$PATH_ORIG"
+    python -m venv venv
+    source venv/$ACTIVATE_DIR/activate
     echo '***'
     python --version
     which python
-    python -m pip --version
+    pip --version
     echo '***'
-    python -m pip install wheel
+    pip install --use-pep517 wheel
     python setup.py build_ext --skip-cmake-build
     python setup.py bdist_wheel --skip-build
+    deactivate
+    rm -rf venv
 done
