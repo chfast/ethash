5fa8e3c180e88dece88b73defc73b38f48df8cd8
Paweł Bylica <pawel@ethereum.org>
2025-02-12 17:21:36 +0100

    skip python 3.9

--- setup.py
+++ setup.py
@@ -93,7 +93,6 @@ setup(
         'Programming Language :: C',
         'Programming Language :: C++',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
